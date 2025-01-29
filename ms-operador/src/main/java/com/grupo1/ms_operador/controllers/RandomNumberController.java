5fc8f9f8a90192bd8db71539661da75f3b60664f
jordan07espi <jeespinosa5@espe.edu.ec>
2025-01-28 20:23:28 -0500

    Comentarioooo

--- ms-operador/src/main/java/com/grupo1/ms_operador/controllers/RandomNumberController.java
+++ ms-operador/src/main/java/com/grupo1/ms_operador/controllers/RandomNumberController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RandomNumberController {
 
+    //localhost:8080/ms-operador/random-number
     @GetMapping("/random-number")
     public int getRandomNumber() {
         return (int) (Math.random() * 100);
