d525de404143fca65698a7577783ddadd32e77c9
AndresToro91 <92707644+AndresToro91@users.noreply.github.com>
2025-02-05 08:25:09 -0500

    Se incluye Swagger

--- ms-operador/src/main/java/com/grupo1/ms_operador/MsOperadorApplication.java
+++ ms-operador/src/main/java/com/grupo1/ms_operador/MsOperadorApplication.java
@@ -3,6 +3,8 @@ package com.grupo1.ms_operador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +12,16 @@ public class MsOperadorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsOperadorApplication.class, args);
+	}
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new io.swagger.v3.oas.models.info.Info()
+						.title("Operador API")
+						.termsOfService("http://swagger.io/terms/")
+						.version("1.0")
+						.license(new io.swagger.v3.oas.models.info.License().name("Apache 2.0").url("http://springdoc.org"))
+						.description("API para gestionar compras de libros"));
 	}
 }
 
--- ms-operador/src/main/java/com/grupo1/ms_operador/payments/infrastructure/controllers/PurchaseController.java
+++ ms-operador/src/main/java/com/grupo1/ms_operador/payments/infrastructure/controllers/PurchaseController.java
@@ -1,5 +1,11 @@
 package com.grupo1.ms_operador.payments.infrastructure.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.grupo1.ms_operador.payments.application.RegisterPurchaseUseCase;
@@ -8,7 +14,6 @@ import com.grupo1.ms_operador.payments.domain.exceptions.BookNotAvailableExcepti
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +25,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/purchases")
+@Tag(name = "Purchases", description = "API for managing book's purchases")
 public class PurchaseController {
 
     @Autowired
@@ -34,29 +40,49 @@ public class PurchaseController {
     @Autowired
     private GetAllPurchasesUseCase getAllPurchasesUseCase;
 
+    @Operation(summary = "Create a new purchase", description = "Endpoint to create a new purchase")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Purchase created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping
-    public ResponseEntity<?> createPurchase(@RequestBody PurchaseDto purchaseDto) {
+    public ResponseEntity<?> createPurchase(@RequestBody(description = "Purchase data", required = true) PurchaseDto purchaseDto) {
         try {
             return ResponseEntity.ok(registerPurchaseUseCase.execute(purchaseDto));
         } catch (BookNotAvailableException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("Error al procesar la compra.");
+            return ResponseEntity.internalServerError().body("Error processing the purchase.");
         }
     }
 
+    @Operation(summary = "Get purchase by ID", description = "Endpoint to get a purchase by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Purchase found"),
+            @ApiResponse(responseCode = "404", description = "Purchase not found")
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<PurchaseDto> getPurchaseById(@PathVariable Long id) {
+    public ResponseEntity<PurchaseDto> getPurchaseById(@Parameter(description = "ID to obtain purchase", required = true) @PathVariable Long id) {
         return ResponseEntity.ok().body(getPurchaseByIdUseCase.execute(id));
     }
 
+    @Operation(summary = "Get all book's purchases", description = "Endpoint to get all purchases")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Purchases obtain successfully")
+    })
     @GetMapping
     public ResponseEntity<?> getAllPurchases() {
         return ResponseEntity.ok().body(getAllPurchasesUseCase.execute());
     }
 
+    @Operation(summary = "Get purchases by book ID", description = "Endpoint to get purchases by book ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Purchases retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Book not found")
+    })
     @GetMapping("/books/{id}")
-    public Object getPurchasesByBookId(@PathVariable("id") Long id) {
+    public Object getPurchasesByBookId(@Parameter(description = "ID of the book to obtain purchases for", required = true) @PathVariable("id") Long id) {
         return this.buscadorClient.getBookById(id);
     }
 }
